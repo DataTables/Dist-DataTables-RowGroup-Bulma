96a2ee2e965242a1aa9650426e877b184f880e23
Allan Jardine <allan@datatables.net>
2022-10-27 15:13:58 +0000

    Update typing information to latest typing integration for DataTables

---
+++ types/types.d.ts
@@ -0,0 +1,21 @@
+
+// Dist-DataTables-RowGroup-Bulma integration with Bulma exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-RowGroup-Bulma integration with Bulma exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-RowGroup-Bulma integration with Bulma exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
