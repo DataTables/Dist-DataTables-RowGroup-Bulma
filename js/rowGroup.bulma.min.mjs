7ad627d3482da3605778d40c20a253be9449619f
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:15:48 +0000

    b089337101a134b448651ef1c1b0d75822424f4f
    Sync to source repo @b089337101a134b448651ef1c1b0d75822424f4f

--- js/rowGroup.bulma.min.mjs
+++ js/rowGroup.bulma.min.mjs
@@ -1,1 +1,4 @@
+/*! Bulma styling wrapper for RowGroup
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-bm";import DataTable from"datatables.net-rowgroup";export default DataTable;
